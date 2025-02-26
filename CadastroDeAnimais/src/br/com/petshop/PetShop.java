3fb1ab0488526622d976d71f44dbdd70e2b30b5d
eupedr0kkj <162990321+eupedr0kkj@users.noreply.github.com>
2025-02-26 08:32:52 -0300

    commitPrimeiro

--- CadastroDeAnimais/src/br/com/petshop/PetShop.java
+++ CadastroDeAnimais/src/br/com/petshop/PetShop.java
@@ -1,9 +1,35 @@
 package br.com.petshop;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class PetShop {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		ArrayList<Animal> animais = new ArrayList<>();
+		int opcao;
+		
+		do {
+			System.out.println("\n=== MENU ===");
+			System.out.println("1 - Cadastrar Cachorro");
+			System.out.println("2 - Cadastrar Gato");
+			System.out.println("3 - Exibir animais");
+			System.out.println("4 - Sair");
+			System.out.print("Escolha uma opção: ");
+			opcao = scanner.nextInt();
+			scanner.nextLine();
+			
+			switch (opcao) {
+			
+			case 1:
+				System.out.print("Nome do cachorro: ");
+				String nomeCachorro = scanner.nextLine();
+				System.out.print("Idade do cachorro: ");
+				int idadeCachorro = scanner.nextInt();
+				scanner
+			}
+			}
 
 	}
 
